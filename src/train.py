867a7e82cf0a1a2229821ef13df9334c1f425c08
Ludvig Joborn <ludvig.joborn@gmail.com>
2022-02-03 16:24:29 +0100

    feat: load desed train, validation and test data

--- src/train.py
+++ src/train.py
@@ -13,20 +13,36 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Test code to plot mel spectrogram
 if True:
-    # Load dataset DESED Public Evaluation
-    DESED = DESED_Strong(
+    # Load dataset DESED Train (Synthetic)
+    DESED_train = DESED_Strong(
+        "DESED Synthetic Training",
+        PATH_TO_SYNTH_TRAIN_DESED_TSV,
+        PATH_TO_SYNTH_TRAIN_DESED_WAVS,
+        DESED_CLIP_LEN_SECONDS,
+    )
+
+    # Load dataset DESED Validation (Synthetic)
+    DESED_val = DESED_Strong(
+        "DESED Synthetic Validation",
+        PATH_TO_SYNTH_VALIDATION_DESED_TSV,
+        PATH_TO_SYNTH_VALIDATION_DESED_WAVS,
+        DESED_CLIP_LEN_SECONDS,
+    )
+
+    # Load dataset DESED Test (Public Evaluation)
+    DESED_test = DESED_Strong(
         "DESED Public Evaluation",
         PATH_TO_PUBLIC_EVAL_DESED_TSV,
         PATH_TO_PUBLIC_EVAL_DESED_WAVS,
-        PUBLIC_EVAL_DESED_CLIP_LEN_SECONDS,
+        DESED_CLIP_LEN_SECONDS,
     )
 
-    print("Name:", DESED)
-    print("length:", len(DESED))
-    print("Sample 0 in DESED:", DESED[0], "\n")
+    print("Name:", DESED_train)
+    print("length:", len(DESED_train))
+    print("Sample 0 in DESED:", DESED_train[0], "\n")
 
     # One sample
-    waveform, sample_rate, labels = DESED.__getitem__(0)
+    waveform, sample_rate, labels = DESED_train.__getitem__(0)
 
     # Parameters for mel spectrogram
     n_fft = 2048  # 1024
@@ -44,4 +60,6 @@ if True:
         plot_spectrogram(melspec[0], title="MelSpectrogram", ylabel="mel freq")
 
     ######################################################
-    DESED_dataloader = DataLoader(DESED, batch_size=64, shuffle=True, pin_memory=True)
+    DESED_dataloader_train = DataLoader(
+        DESED_train, batch_size=64, shuffle=True, pin_memory=True
+    )
