2b8d78e0375530fb89d7126b2453cf89660a65f8
Ludvig Joborn <ludvig.joborn@gmail.com>
2022-02-03 16:24:29 +0100

    feat: load desed train, validation and test data

--- src/config.py
+++ src/config.py
@@ -13,15 +13,33 @@ CLIP_LEN_SECONDS = 10
 
 ### DESED ###
 # Clip length of audio files
-PUBLIC_EVAL_DESED_CLIP_LEN_SECONDS = CLIP_LEN_SECONDS
+DESED_CLIP_LEN_SECONDS = CLIP_LEN_SECONDS
 
+# Train Dataset
+PATH_TO_SYNTH_TRAIN_DESED_TSV = Path(
+    "C:/Users/ludvi/Desktop/Thesis-Project/data/dcase21_synth/metadata/train/synhtetic21_train/soundscapes.tsv"
+)
+PATH_TO_SYNTH_TRAIN_DESED_WAVS = Path(
+    "C:/Users/ludvi/Desktop/Thesis-Project/data/dcase21_synth/audio/train/synthetic21_train/soundscapes"
+)
+
+# Validation Dataset
+PATH_TO_SYNTH_VALIDATION_DESED_TSV = Path(
+    "C:/Users/ludvi/Desktop/Thesis-Project/data/dcase21_synth/metadata/validation/synhtetic21_validation/soundscapes.tsv"
+)
+PATH_TO_SYNTH_VALIDATION_DESED_WAVS = Path(
+    "C:/Users/ludvi/Desktop/Thesis-Project/data/dcase21_synth/audio/validation/synthetic21_validation/soundscapes"
+)
+
+# Test Dataset
 PATH_TO_PUBLIC_EVAL_DESED_TSV = Path(
-    "E:/Datasets/desed_zenodo/DESEDpublic_eval/dataset/metadata/eval/public.tsv"
+    # "E:/Datasets/desed_zenodo/DESEDpublic_eval/dataset/metadata/eval/public.tsv"
+    "C:/Users/ludvi/Desktop/Thesis-Project/data/desed_real_eval/metadata/eval/public.tsv"
 )
 PATH_TO_PUBLIC_EVAL_DESED_WAVS = Path(
-    "E:/Datasets/desed_zenodo/DESEDpublic_eval/dataset/audio/eval/public"
+    # "E:/Datasets/desed_zenodo/DESEDpublic_eval/dataset/audio/eval/public"
+    "C:/Users/ludvi/Desktop/Thesis-Project/data/desed_real_eval/audio/eval/public"
 )
-
 
 ##############
 ### MODELS ###
