4cd24b48e86a1d5f1c8c520c9df0d81b5e5bdaed
Mattias Beming <mattias.beming@outlook.com>
2022-02-03 14:58:49 +0100

    feat: train.py code to load desed dataset and plot spectrogram

---
+++ src/melspec.py
@@ -0,0 +1,61 @@
+import torch
+import torchaudio.transforms as T
+import matplotlib.pyplot as plt
+import librosa
+from typing import Callable
+
+
+def plot_spectrogram(spec, title=None, ylabel="freq_bin", aspect="auto", xmax=None):
+    fig, axs = plt.subplots(1, 1)
+    axs.set_title(title or "Spectrogram (db)")
+    axs.set_ylabel(ylabel)
+    axs.set_xlabel("frame")
+    im = axs.imshow(librosa.power_to_db(spec), origin="lower", aspect=aspect)
+    if xmax:
+        axs.set_xlim((0, xmax))
+    fig.colorbar(im, ax=axs)
+    plt.show(block=True)
+
+
+def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None):
+    waveform = waveform.numpy()
+
+    num_channels, num_frames = waveform.shape
+    time_axis = torch.arange(0, num_frames) / sample_rate
+
+    figure, axes = plt.subplots(num_channels, 1)
+    if num_channels == 1:
+        axes = [axes]
+    for c in range(num_channels):
+        axes[c].plot(time_axis, waveform[c], linewidth=1)
+        axes[c].grid(True)
+        if num_channels > 1:
+            axes[c].set_ylabel(f"Channel {c+1}")
+        if xlim:
+            axes[c].set_xlim(xlim)
+        if ylim:
+            axes[c].set_ylim(ylim)
+    figure.suptitle(title)
+    plt.show(block=True)
+
+
+def get_mel_spectrogram(
+    sample_rate: int, n_fft: int, win_length: int, hop_length: int, n_mels: int
+) -> Callable:
+    """
+    Returns a function that takes a waveform and returns a mel spectrogram.
+    """
+    mel_spectrogram = T.MelSpectrogram(
+        sample_rate=sample_rate,
+        n_fft=n_fft,
+        win_length=win_length,
+        hop_length=hop_length,
+        center=True,
+        pad_mode="reflect",
+        power=2.0,
+        norm="slaney",
+        onesided=True,
+        n_mels=n_mels,
+        mel_scale="htk",
+    )
+    return mel_spectrogram
