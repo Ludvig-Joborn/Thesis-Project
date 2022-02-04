f2eb976d7851ed0edb7acebd092570f74b5f959e
Mattias Beming <mattias.beming@outlook.com>
2022-02-04 13:29:43 +0100

    feat: downsampling layer in basic nn

--- src/models/basic_nn.py
+++ src/models/basic_nn.py
@@ -1,15 +1,32 @@
 import os
 import torch
 from torch import nn
+<<<<<<< HEAD
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+=======
+from nnAudio.features.mel import MelSpectrogram
+import julius
+
+# User defined imports
+from config import PARAMS_TO_MELSPEC, SAMPLE_RATE
+>>>>>>> d322581... feat: downsampling layer in basic nn
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Using {device} device')
 
 class NeuralNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, input_sample_rate: int, output_sample_rate: int = SAMPLE_RATE):
         super(NeuralNetwork, self).__init__()
+
+        # layer that downsamples the waveform to lower sample rate
+        self.downsampler = julius.resample.ResampleFrac(
+            input_sample_rate, output_sample_rate
+        )
+
+        # layer that converts waveforms to mel spectrograms
+        self.spec_layer = MelSpectrogram(**PARAMS_TO_MELSPEC)
+
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(128*313, 4096),
@@ -19,7 +36,9 @@ class NeuralNetwork(nn.Module):
             nn.Linear(512, 2),
         )
 
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+    def forward(self, waveform: torch.Tensor):
+        waveform_ds = self.downsampler(waveform)
+        mel_spec = self.spec_layer(waveform_ds)
+        z = self.flatten(mel_spec)
+        logits = self.linear_relu_stack(z)
         return logits
