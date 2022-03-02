3f5d4993c5b2a74d743a5104e5dc2fab1007154f
Ludvig Joborn <ludvig.joborn@gmail.com>
2022-03-02 17:05:10 +0100

    refactor: moving functions between eval.py and train.py

--- src/eval.py
+++ src/eval.py
@@ -10,8 +10,8 @@ from config import *
 from utils import *
 from models.model_utils import *
 from logger import CustomLogger as Logger
-from models.model_extensions_1.b2 import NeuralNetwork as NN
 from datasets.dataset_handler import DatasetWrapper
+from models.baseline import NeuralNetwork as NN
 
 # Use cuda if available, exit otherwise
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -19,26 +19,31 @@ if device == "cpu":
     exit("Please use a GPU to train this model.")
 
 
-def test(
-    te_loader: DataLoader, len_te: int, model, criterion, log: Logger
-) -> Tuple[List[float], List[float]]:
+def te_val_batches(
+    data_loader: DataLoader,
+    len_data: int,
+    model,
+    criterion,
+    testing: bool = False,
+) -> Tuple[float, torch.Tensor, int, int]:
     """
-    Test loop. Calculates test loss and accuracy.
+    Runs batches for the dataloader sent in without updating any model weights.
+    Used for calculating validation and test accuracy and loss.
     """
-    # Track test loss
-    test_loss = 0
+    # Track loss
+    total_loss = 0
 
-    # Track correct and total predictions to calculate test accuracy
-    correct = torch.zeros(-(len_te // -BATCH_SIZE)).to(device, non_blocking=True)
+    # Track correct and total predictions to calculate epoch accuracy
+    correct = torch.zeros(-(len_data // -BATCH_SIZE)).to(device, non_blocking=True)
     total = 0
 
     # Set model state to evaluation
     model.eval()
 
+    ##### Evaluate on validation dataset #####
     with torch.no_grad():
-        for i, sample in enumerate(
-            tqdm(iterable=te_loader, desc="Test Batch progress:")
-        ):
+        desc = "Test Batch progress:" if testing else "Valdiation Batch progress:"
+        for i, sample in enumerate(tqdm(iterable=data_loader, desc=desc)):
             waveform, labels = sample
 
             # Send parameters to device
@@ -50,13 +55,36 @@ def test(
             loss = criterion(outputs, labels)
 
             # Track loss statistics
-            test_loss += loss
+            total_loss += loss
             correct[i], total = update_acc(activation(outputs), labels, total)
+
+        return total_loss, correct, total, i
+
+
+def test(
+    te_loader: DataLoader,
+    len_te: int,
+    model,
+    criterion,
+    log: Logger,
+    testing: bool = False,
+) -> Tuple[float, float]:
+    """
+    Test loop. Calculates test loss and accuracy with no weight updates.
+    """
+
+    ##### Run test batches #####
+    test_loss, correct, total, total_te_batches = te_val_batches(
+        te_loader, len_te, model, criterion, testing
+    )
 
     # Log test loss and accuracy
     test_loss = test_loss.item()
     test_acc = correct.sum().item() / total
-    log.info(f"Average testing loss: {test_loss/(i+1)}", display_console=False)
+    log.info(
+        f"Average testing loss: {test_loss/(total_te_batches+1)}",
+        display_console=False,
+    )
     log.info(f"Average test accuracy: {test_acc}", display_console=True)
 
     return test_loss, test_acc
@@ -83,15 +111,15 @@ if __name__ == "__main__":
 
     # Network
     model = NN(sample_rate, SAMPLE_RATE).to(device, non_blocking=True)
-    summary(model, input_size=(BATCH_SIZE, 1, 10 * SAMPLE_RATE), device=device)
+    # summary(model, input_size=(BATCH_SIZE, 1, 10 * sample_rate), device=device)
 
     # Loss function
     criterion = nn.BCELoss()
 
     # Load model from disk
     state = load_model(LOAD_MODEL_PATH)
-    log.info(f"Loaded model from {LOAD_MODEL_PATH}")
     model.load_state_dict(state["state_dict"])
+    log.info(f"Loaded model from {LOAD_MODEL_PATH}")
 
     # Number of trained parameters in network
     log.info(f"Number of trained parameters: {nr_parameters(model)}")
@@ -99,7 +127,7 @@ if __name__ == "__main__":
     ### Test Model ###
     log.info("Testing", add_header=True)
 
-    te_loss, te_acc = test(DS_test_loader, len_te, model, criterion, log)
+    te_loss, te_acc = test(DS_test_loader, len_te, model, criterion, log, testing=True)
 
     # Dictionary with paths, losses and accuracies
     model_save = state["model_save"]
