900b90b3c30de59431887b199906b07009cb09ec
Mattias Beming <mattias.beming@outlook.com>
2022-03-02 15:11:25 +0100

    feat: new model-class for multiple conv layers

--- src/models/conv_block.py
+++ src/models/conv_block.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from collections import OrderedDict
 
 # User defined imports
 from models.model_utils import ACT, POOL
@@ -67,3 +68,59 @@ class ConvBlock(nn.Module):
         act = self.act(pool)
         bn = self.bn(act)
         return bn
+
+
+class MultipleConvLayers(nn.Module):
+    def __init__(
+        self,
+        number_layers=1,
+        inC=16,
+        outC=32,
+        c_ks=(3, 3),
+        c_stride=(1, 1),
+        c_padding="same",
+        act=ACT.RELU,
+        dropout=0,
+    ):
+        super(MultipleConvLayers, self).__init__()
+
+        # Create layers
+        layers = OrderedDict()
+        for i in range(number_layers):
+            if i == 1:
+                inC = outC
+            layers[str(i)] = self.layer(
+                inC, outC, c_ks, c_stride, c_padding, act, dropout
+            )
+        self.stacked = nn.Sequential(layers)
+
+    def layer(self, inC, outC, c_ks, c_stride, c_padding, act, dropout):
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels=inC,
+                out_channels=outC,
+                kernel_size=c_ks,
+                stride=c_stride,
+                padding=c_padding,
+            ),
+            nn.Dropout(p=dropout),
+            self.act_func(act),
+            nn.BatchNorm2d(outC),
+        )
+
+    def act_func(self, act=ACT.GLU):
+        if act == ACT.GLU:
+            return nn.GLU(dim=2)
+        elif act == ACT.RELU:
+            return nn.ReLU()
+        elif act == ACT.LS_RELU:
+            return LS_ReLU()
+        elif act == ACT.SWISH:
+            return SWISH()
+        elif act == ACT.LS_RELU_TR:
+            return LS_ReLU(trainable=True)
+        elif act == ACT.SWISH_TR:
+            return SWISH(trainable=True)
+
+    def __call__(self, input: torch.Tensor):
+        return self.stacked(input)
