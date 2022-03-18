8d472926ff22ef5d3bf569ebf9c88db6c07b4aa9
Mattias Beming <mattias.beming@outlook.com>
2022-03-18 11:31:20 +0100

    refactor: old cbam which works with old trained models

--- src/models/cbam.py
+++ src/models/cbam.py
@@ -11,10 +11,10 @@ https://github.com/xmu-xiaoma666/External-Attention-pytorch/blob/master/model/at
 class ChannelAttention(nn.Module):
     def __init__(self, inC, reduction=16):
         super(ChannelAttention, self).__init__()
-        self.max_pool = nn.AdaptiveMaxPool2d(1)
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
 
-        self.se = nn.Sequential(
+        self.fc = nn.Sequential(
             nn.Conv2d(
                 in_channels=inC,
                 out_channels=inC // reduction,
@@ -32,25 +32,30 @@ class ChannelAttention(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def __call__(self, x):
-        max_res = self.se(self.max_pool(x))
-        avg_res = self.se(self.avg_pool(x))
-        return self.sigmoid(max_res + avg_res)
+        avg_out = self.fc(self.avg_pool(x))
+        max_out = self.fc(self.max_pool(x))
+        out = avg_out + max_out
+        return self.sigmoid(out)
 
 
 class SpatialAttention(nn.Module):
     def __init__(self, c_ks=7):
         super(SpatialAttention, self).__init__()
 
-        self.conv = nn.Conv2d(
-            in_channels=2, out_channels=1, kernel_size=c_ks, padding=c_ks // 2
+        self.conv1 = nn.Conv2d(
+            in_channels=2,
+            out_channels=1,
+            kernel_size=c_ks,
+            padding=c_ks // 2,
+            bias=False,
         )
         self.sigmoid = nn.Sigmoid()
 
     def __call__(self, x):
-        max_res, _ = torch.max(x, dim=1, keepdim=True)
-        avg_res = torch.mean(x, dim=1, keepdim=True)
-        res = torch.cat([max_res, avg_res], dim=1)
-        out = self.conv(res)
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        res = torch.cat([avg_out, max_out], dim=1)
+        out = self.conv1(res)
         return self.sigmoid(out)
 
 
