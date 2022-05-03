18ca477e86dcf93c9d42ef218bb21874592a8344
Mattias Beming <mattias.beming@outlook.com>
2022-05-03 09:16:33 +0200

    feat: files for getting dataset info and plots for SRs

---
+++ src/TEMP_get_time_speech.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+p_desed_real = Path(
+    "E:/Datasets/DESED_REAL_DOWNLOAD/DESED/real/metadata/validation/validation.tsv"
+)
+PATH_TO_SYNTH_TRAIN_DESED_TSV = Path(
+    "E:/Datasets/desed_zenodo/V3/dcase21_synth/metadata/train/synhtetic21_train/soundscapes.tsv"
+)
+PATH_TO_SYNTH_VAL_DESED_TSV = Path(
+    "E:/Datasets/desed_zenodo/V3/dcase21_synth/metadata/validation/synhtetic21_validation/soundscapes.tsv"
+)
+PATH_TO_PUBLIC_EVAL_DESED_TSV = Path(
+    "E:/Datasets/desed_zenodo/DESEDpublic_eval/dataset/metadata/eval/public.tsv"
+)
+annotations = pd.read_table(PATH_TO_PUBLIC_EVAL_DESED_TSV)
+
+
+# Unique filenames
+a_ = len(annotations["filename"].unique())
+
+# Get all rows with speech
+df_speech = annotations.loc[annotations["event_label"] == "Speech"]
+sum_speech = 0.0
+for index, row in df_speech.iterrows():
+    sum_speech += row["offset"] - row["onset"]
+
+# Prints
+print("nr unique filenames:", a_)
+print(f"-> Total minutes in dataset: {a_ * 10.0:1f} | hours: {a_ * 10.0/60:1f}")
+print(f"-> Total minutes of speech: {sum_speech:1f} | hours: {sum_speech/60:1f}")
+print(f"Speech: {sum_speech / (a_ * 10.0):2f}")
---
+++ src/TEMP_plot_SRs.py
@@ -0,0 +1,229 @@
+import torch
+import torchaudio.transforms as T
+import matplotlib.pyplot as plt
+import librosa
+from torch.utils.data import Dataset
+from typing import Callable, List, Dict
+import numpy as np
+from pathlib import Path
+from tqdm import tqdm
+
+# User-defined imports
+import config
+from models.model_utils import load_model
+from utils import get_datetime, psd_score
+from models_dict import Model
+from logger import CustomLogger as Logger
+
+import torch
+import matplotlib.pyplot as plt
+from typing import Callable, List, Dict
+import numpy as np
+from pathlib import Path
+from enum import Enum
+from tqdm import tqdm
+import pandas as pd
+import warnings
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+import collections
+
+# User-defined imports
+import config
+from models_dict import *
+from datasets.dataset_handler import DatasetManager
+from models.model_utils import *
+from eval import *
+from utils import timer
+import time
+
+# Ignore warnings
+warnings.simplefilter("ignore", UserWarning)
+
+
+def get_val_model_value_epoch(
+    state,
+    act_threshold: float = config.ACT_THRESHOLD,
+    DS_val: Dataset = None,
+    psds_params: Dict = config.PSDS_PARAMS,
+):
+    operating_points = config.OPERATING_POINTS
+    # Get the threshold value in 'operating_points' that is closest to 'act_threshold'
+    used_threshold = min(
+        operating_points, key=lambda input_list: abs(input_list - act_threshold)
+    )
+
+    op_table = state["op_table"]
+    psds, fscores = psd_score(
+        op_table,
+        DS_val.get_annotations(),
+        psds_params,
+        operating_points,
+    )
+
+    values = {}
+    try:
+        values["Fscores"] = fscores.Fscores.loc[used_threshold]
+    except:
+        values["Fscores"] = 0
+
+    try:
+        values["psds"] = psds.value
+    except:
+        values["psds"] = 0
+
+    # Add values
+    values["tr_epoch_accs"] = state["tr_epoch_accs"][-1]
+    values["tr_epoch_losses"] = state["tr_epoch_losses"][-1]
+    values["val_acc_table"] = state["val_acc_table"][used_threshold][-1]
+    values["val_epoch_losses"] = state["val_epoch_losses"][-1]
+
+    return values
+
+
+DS_train_name = config.DESED_SYNTH_TRAIN_ARGS["name"]
+DS_train_basepath = Path(config.SAVED_MODELS_DIR) / DS_train_name
+
+DM = DatasetManager()
+params_DS_val = config.DESED_SYNTH_VAL_ARGS
+DS_val_loader = DM.load_dataset(**params_DS_val)
+DS_val_name = params_DS_val["name"]
+DS_val = DM.get_dataset(DS_val_name)
+
+
+PSDS_PARAMS_01 = {
+    "duration_unit": "hour",
+    "dtc_threshold": 0.1,
+    "gtc_threshold": 0.1,
+    "cttc_threshold": 0.1,
+    "alpha_ct": 0.0,
+    "alpha_st": 0.0,
+    "max_efpr": 100,
+}
+
+PSDS_PARAMS_07 = {
+    "duration_unit": "hour",
+    "dtc_threshold": 0.7,
+    "gtc_threshold": 0.7,
+    "cttc_threshold": 0.1,
+    "alpha_ct": 0.0,
+    "alpha_st": 0.0,
+    "max_efpr": 100,
+}
+
+OP_THRESHOLD = 0.5
+EPOCHS = 20
+
+
+def _G_(val_model_basepath, PSDS_PARAMS=PSDS_PARAMS_01):
+    d_ = f"{PSDS_PARAMS['dtc_threshold']}_model epoch"
+    values_all_epochs = []
+    for epoch in tqdm(range(1, EPOCHS + 1), desc=d_, position=1, leave=False):
+        state = load_model(val_model_basepath / f"Ve{epoch}.pt")
+        vals = get_val_model_value_epoch(state, OP_THRESHOLD, DS_val, PSDS_PARAMS)
+        values_all_epochs.append(vals)
+    return values_all_epochs
+
+
+WHAT_TO_PLOT = [
+    config.PLOT_MODES.TR_ACC,
+    config.PLOT_MODES.TR_LOSS,
+    config.PLOT_MODES.VAL_ACC,
+    config.PLOT_MODES.VAL_LOSS,
+    config.PLOT_MODES.PSDS,
+    config.PLOT_MODES.FSCORE,
+]
+
+s_time = time.time()
+plot_vals_01_bs = {}
+plot_vals_07_bs = {}
+plot_vals_01_imp_bs = {}
+plot_vals_07_imp_bs = {}
+
+SRs = [8000, 16000, 22050, 44100]
+for SR in tqdm(SRs, desc="SR", position=0):
+    p_bs = Path(
+        f"E:/saved_models/seed_12345/SR{SR}_lr002_M08_G09/{DS_train_name}/baseline"
+    )
+    p_imp_bs = Path(
+        f"E:/saved_models/seed_12345/SR{SR}_lr002_M08_G09/{DS_train_name}/improved_baseline"
+    )
+    p_imp_bs_ks33_l22_gru_2 = Path(
+        f"E:/saved_models/seed_12345/SR{SR}_lr002_M08_G09/{DS_train_name}/b_ks33_l22_gru_2"
+    )
+
+    # bs
+    val_model_basepath_bs = p_bs / "validation" / str(DS_val)
+    values_all_epochs_01_bs = _G_(val_model_basepath_bs, PSDS_PARAMS_01)
+    values_all_epochs_07_bs = _G_(val_model_basepath_bs, PSDS_PARAMS_07)
+
+    plot_vals_01_bs[f"baseline_{SR}"] = values_all_epochs_01_bs
+    plot_vals_07_bs[f"baseline_{SR}"] = values_all_epochs_07_bs
+
+    if SR == 16000:
+        # imp. bs (b_ks33_l22_gru_2)
+        val_model_basepath_imp_bs = p_imp_bs_ks33_l22_gru_2 / "validation" / str(DS_val)
+        values_all_epochs_01_imp_bs = _G_(val_model_basepath_imp_bs, PSDS_PARAMS_01)
+        values_all_epochs_07_imp_bs = _G_(val_model_basepath_imp_bs, PSDS_PARAMS_07)
+    else:
+        # imp. bs
+        val_model_basepath_imp_bs = p_imp_bs / "validation" / str(DS_val)
+        values_all_epochs_01_imp_bs = _G_(val_model_basepath_imp_bs, PSDS_PARAMS_01)
+        values_all_epochs_07_imp_bs = _G_(val_model_basepath_imp_bs, PSDS_PARAMS_07)
+
+    plot_vals_01_imp_bs[f"improved_baseline_{SR}"] = values_all_epochs_01_imp_bs
+    plot_vals_07_imp_bs[f"improved_baseline_{SR}"] = values_all_epochs_07_imp_bs
+
+
+print(f"Took: {timer(s_time, time.time())}")
+
+
+def di_c_T(list_of_dicts):
+    dict_ = list_of_dicts[0]
+    master_dict = {key: [] for key in dict_.keys()}
+    for dict_ in list_of_dicts:
+        for key, val in dict_.items():
+            master_dict[key].append(val)
+    return master_dict
+
+
+def plot_models(what_to_plot: List[config.PLOT_MODES], plot_vals, title):
+    # Parameters for plotting
+    plot_params = {
+        "linestyle": "-",
+        "marker": "o",
+        "markersize": 4,
+    }
+    plt.style.use("ggplot")
+    plt.figure(title, figsize=(18, 10))
+
+    # Determine plot-grid
+    if len(what_to_plot) == 1:
+        plot_grid = (1, 1)
+    else:
+        plot_grid = (2, -(-len(what_to_plot) // 2))
+
+    ### Plots ###
+    x = range(1, EPOCHS + 1)
+    for i, wtp in enumerate(what_to_plot):
+        for model_name, list_of_dicts in plot_vals.items():
+            # print("list_of_dicts\n", list_of_dicts)
+            values = di_c_T(list_of_dicts)
+            # print("values\n", values)
+            plt.subplot(plot_grid[0], plot_grid[1], i + 1)
+            plt.plot(list(x), values[wtp.value[0]], **plot_params, label=model_name)
+            plt.ylabel(wtp.value[1])
+            plt.xlabel("Epoch")
+            plt.title(wtp.value[2])
+            plt.legend()
+
+    plt.show()
+
+
+plot_models(WHAT_TO_PLOT, plot_vals_01_bs, "PSDS 0.1")
+
+plot_models(WHAT_TO_PLOT, plot_vals_07_bs, "PSDS 0.7")
+
+plot_models(WHAT_TO_PLOT, plot_vals_01_imp_bs, "PSDS 0.1")
+
+plot_models(WHAT_TO_PLOT, plot_vals_07_imp_bs, "PSDS 0.7")
